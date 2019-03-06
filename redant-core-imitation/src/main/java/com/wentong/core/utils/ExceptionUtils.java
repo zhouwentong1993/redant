fd61da4aef4c760e5264926174e2f848667aad81
周文童 <zhouwentong@finupgroup.com>
2019-03-06 19:28:55 +0800

    add exception

--- redant-core-imitation/src/main/java/com/wentong/core/utils/ExceptionUtils.java
+++ redant-core-imitation/src/main/java/com/wentong/core/utils/ExceptionUtils.java
@@ -1,7 +1,10 @@
 package com.wentong.core.utils;
 
+import com.wentong.core.exception.CommonException;
+
 public final class ExceptionUtils {
 
-
-
+    public void warring(String message) {
+        throw new CommonException(message);
+    }
 }
